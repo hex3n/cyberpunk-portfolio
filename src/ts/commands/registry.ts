65d4811af6c15af160c8f7bb4f714b107b261ac4
Slowbro213 <thanas.papa.24@gmail.com>
2025-10-23 20:10:13 +0200

    build

---
+++ src/ts/commands/registry.ts
@@ -0,0 +1,12 @@
+// AUTO-GENERATED FILE — DO NOT EDIT MANUALLY
+
+import type { Command } from "./types";
+import { help } from "./help";
+import { echo } from "./echo";
+import { ls } from "./ls";
+import { cd } from "./cd";
+import { slowking } from "./slowking";
+import { clear } from "./clear";
+import { history } from "./history";
+
+export const commands: Command[] = [help, echo, ls, cd, slowking, clear, history];
